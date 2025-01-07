1023d702413c351ebb110493aeed8cf3b0b24cc8
Bruno Tomassetti Couto <bruno.t.couto@icloud.com>
2025-01-07 19:12:52 -0300

    fix: Update balena-cli installation path in deploy script

--- .github/scripts/deploy-balena.sh
+++ .github/scripts/deploy-balena.sh
@@ -27,28 +27,11 @@ fi
 
 # Metadata
 draft=false
-tag="$(git describe --tags --exact-match HEAD 2>/dev/null || true)"
 SHA="$(git rev-parse HEAD)"
 
-# If not on a tag, then we create a draft which isn't automatically deployed to devices but still usable.
-if [[ $tag == "" ]]; then
-  draft=true
-fi
+echo "Starting balena deployment: draft=$draft, SHA='$SHA'"
 
-echo "Starting balena deployment: draft=$draft, tag='$tag', SHA='$SHA'"
-
-tags=("sha" "$SHA")
-
-# args=("$FLEET" "--nocache" "--registry-secrets" "/tmp/image_pull_secrets.yaml")
 args=("$FLEET" "--nocache")
-
-if [[ $draft == true ]]; then
-  args+=("--draft")
-else
-  tags+=("version" "$tag")
-fi
-
-args+=("--release-tag" "${tags[@]}")
 
 echo "Logging in to balena cloud"
 balena login --token "$BALENA_TOKEN"
